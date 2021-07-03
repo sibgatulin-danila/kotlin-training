fe6d1a850d8d22bbbfab60c4401acfbe13101b13
D <sibgat.d.s@gmail.com>
2021-07-03 18:57:45 +0700

    added error handling

--- TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -20,7 +20,11 @@ class MainActivity : AppCompatActivity() {
 
     fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            return
+        }
+
         val selectedId = binding.tipOptions.checkedRadioButtonId
         val tipPercentage = when (selectedId) {
             R.id.serviceQualityAwesome -> 0.20
